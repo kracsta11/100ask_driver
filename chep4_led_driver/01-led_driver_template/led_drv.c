9d2ed1cec776c3bbe2f62fb25423d8719e7c4c6f
kracstal <15261009340@163.com>
2024-07-05 04:47:10 -0400

    add led driver framework

--- chep4_led_driver/01-led_driver_template/led_drv.c
+++ chep4_led_driver/01-led_driver_template/led_drv.c
@@ -25,17 +25,18 @@
  7. 完善 提供设备信息 自动创建设备节点
 ***************************************************************/
 
-#define BUF_SIZE (1024)
+#define LED_NUM  (2)
+
 #define MIN(a,b) (a < b ? a : b)
 
 
 /***************************************************************
   函数声明
  ***************************************************************/
-static int      __HelloOpen(struct inode *pnode, struct file *pfile);
-static ssize_t  __HelloRead(struct file *pfile, char __user * cbuf, size_t size, loff_t *poffset);
-static ssize_t  __HelloWrite(struct file *pfile, const char __user *cbuf, size_t size, loff_t *poffset);
-static int      __HelloClose(struct inode *pnode, struct file *pfile);
+static int      __LedOpen(struct inode *pnode, struct file *pfile);
+static ssize_t  __LedRead(struct file *pfile, char __user * cbuf, size_t size, loff_t *poffset);
+static ssize_t  __LedWrite(struct file *pfile, const char __user *cbuf, size_t size, loff_t *poffset);
+static int      __LedClose(struct inode *pnode, struct file *pfile);
 
 /*************************************************************** 
   全局变量定义
@@ -47,12 +48,12 @@ static int      __HelloClose(struct inode *pnode, struct file *pfile);
  /*
  * 定义file_ops结构体
  */
- static const struct file_operations hello_fops = {
+ static const struct file_operations led_fops = {
 	.owner		= THIS_MODULE,
-	.open		= __HelloOpen,
-	.read		= __HelloRead,
-    .write      = __HelloWrite,
-    .release    = __HelloClose,
+	.open		= __LedOpen,
+	.read		= __LedRead,
+    .write      = __LedWrite,
+    .release    = __LedClose,
 };
 /*
  * 定义驱动数据缓存区
@@ -61,10 +62,10 @@ static int      __HelloClose(struct inode *pnode, struct file *pfile);
 /*
  * 定义一个类
  */
-static struct class *_G_phello_class;
+static struct class *_G_pled_class;
 
 /*********************************************************************************************************
-** 函数名称: __HelloOpen
+** 函数名称: __LedOpen
 ** 功能描述: 驱动打开
 ** 输　入  : pnode           节点指针
 **           pfile           文件指针
@@ -72,13 +73,13 @@ static struct class *_G_phello_class;
 ** 全局变量:
 ** 调用模块:
 *********************************************************************************************************/
-static int __HelloOpen(struct inode *pnode, struct file *pfile)
+static int __LedOpen(struct inode *pnode, struct file *pfile)
 {
     printk("%s %s: line%d.\n", __FILE__, __FUNCTION__, __LINE__);
     return 0;
 }
 /*********************************************************************************************************
-** 函数名称: __HelloRead
+** 函数名称: __LedRead
 ** 功能描述: 驱动读
 ** 输　入  : pfile           文件指针
 **           cbuf            来自用户层的数据缓存
@@ -88,14 +89,14 @@ static int __HelloOpen(struct inode *pnode, struct file *pfile)
 ** 全局变量:
 ** 调用模块:
 *********************************************************************************************************/
-static ssize_t  __HelloRead(struct file *pfile, char __user * cbuf, size_t size, loff_t *poffset)
+static ssize_t  __LedRead(struct file *pfile, char __user * cbuf, size_t size, loff_t *poffset)
 {
+    int iErr;
     printk("%s %s: line%d.\n", __FILE__, __FUNCTION__, __LINE__);
-    copy_to_user(cbuf, _G_ckbuf, MIN(BUF_SIZE, size));
     return MIN(BUF_SIZE, size);
 }
 /*********************************************************************************************************
-** 函数名称: __HelloWrite
+** 函数名称: __LedWrite
 ** 功能描述: 驱动写
 ** 输　入  : pfile           文件指针
 **           cbuf            来自用户层的数据缓存
@@ -105,14 +106,18 @@ static ssize_t  __HelloRead(struct file *pfile, char __user * cbuf, size_t size,
 ** 全局变量:
 ** 调用模块:
 *********************************************************************************************************/
-static ssize_t  __HelloWrite(struct file *pfile, const char __user *cbuf, size_t size, loff_t *poffset)
+static ssize_t  __LedWrite(struct file *pfile, const char __user *cbuf, size_t size, loff_t *poffset)
 {
-    printk("%s %s: line%d.\n", __FILE__, __FUNCTION__, __LINE__);
-    copy_from_user(_G_ckbuf, cbuf, MIN(BUF_SIZE, size));
-    return MIN(BUF_SIZE, size);
+    int iErr;
+    char cStatus;
+    iErr = copy_from_user(&cStatus, cbuf, 1);
+    
+    /* 根据次设备号和status控制led */
+
+    return 1;
 }
 /*********************************************************************************************************
-** 函数名称: __HelloClose
+** 函数名称: __LedClose
 ** 功能描述: 驱动关闭
 ** 输　入  : pnode           节点指针
 **           pfile           文件指针
@@ -120,14 +125,14 @@ static ssize_t  __HelloWrite(struct file *pfile, const char __user *cbuf, size_t
 ** 全局变量:
 ** 调用模块:
 *********************************************************************************************************/
-static int      __HelloClose(struct inode *pnode, struct file *pfile)
+static int      __LedClose(struct inode *pnode, struct file *pfile)
 {
     printk("%s %s: line%d.\n", __FILE__, __FUNCTION__, __LINE__);
     return 0;
 }
 
 /*********************************************************************************************************
-** 函数名称: HelloInit
+** 函数名称: ledInit
 ** 功能描述: 驱动初始化
 ** 输　入  : pnode           节点指针
 **           pfile           文件指针
@@ -135,23 +140,28 @@ static int      __HelloClose(struct inode *pnode, struct file *pfile)
 ** 全局变量:
 ** 调用模块:
 *********************************************************************************************************/
-int HelloInit(void)
+int ledInit(void)
 {
+    int iErr;
+    int i;
     printk("%s %s: line%d.\n", __FILE__, __FUNCTION__, __LINE__);
-    _G_imajor = register_chrdev(0, "hello", &hello_fops);
+    _G_imajor = register_chrdev(0, "my_led", &led_fops);
 
-    _G_phello_class = class_create(THIS_MODULE, "hello_class");
-	if (IS_ERR(_G_phello_class)){
+    _G_pled_class = class_create(THIS_MODULE, "led_class");
+	if (IS_ERR(_G_pled_class)){
         printk("%s %s: line%d.\n", __FILE__, __FUNCTION__, __LINE__);
-        unregister_chrdev(_G_imajor, "hello");
+        unregister_chrdev(_G_imajor, "my_led");
         return -1;
     }
-    device_create(_G_phello_class, NULL, MKDEV(_G_imajor, 0), NULL, "hello");
+    for(i = 0; i < LED_NUM; i++){
+        device_create(_G_pled_class, NULL, MKDEV(_G_imajor, i), NULL, "my_led%d", i);
+    }
+    
     return 0;
 }
 
 /*********************************************************************************************************
-** 函数名称: HelloExit
+** 函数名称: ledExit
 ** 功能描述: 驱动注销
 ** 输　入  : pnode           节点指针
 **           pfile           文件指针
@@ -159,19 +169,22 @@ int HelloInit(void)
 ** 全局变量:
 ** 调用模块:
 *********************************************************************************************************/
-void HelloExit(void)
+void ledExit(void)
 {
+    int i;
     printk("%s %s: line%d.\n", __FILE__, __FUNCTION__, __LINE__);
-    device_destroy(_G_phello_class, MKDEV(_G_imajor, 0));
-    class_destroy(_G_phello_class);
-    unregister_chrdev(_G_imajor, "hello");
+    fir(i = 0; i < LED_NUM; i++){
+        device_destroy(_G_pled_class, MKDEV(_G_imajor, i));
+    }
+    class_destroy(_G_pled_class);
+    unregister_chrdev(_G_imajor, "my_led");
 
     return 0;
 }
 
-module_init(HelloInit);
-module_exit(HelloExit);
+module_init(ledInit);
+module_exit(ledExit);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Hello Driver");
+MODULE_DESCRIPTION("led Driver");
 MODULE_AUTHOR("Pan Yang");
